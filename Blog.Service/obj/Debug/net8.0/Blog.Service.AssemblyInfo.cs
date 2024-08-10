c9c53e4dda95a6d9e7804fd245d20fd8f14d5fac
MuradMT <mammadzade03@gmail.com>
2024-08-10 15:45:06 +0400

    Added sneat admin panel template.

--- Blog.Service/obj/Debug/net8.0/Blog.Service.AssemblyInfo.cs
+++ Blog.Service/obj/Debug/net8.0/Blog.Service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog.Service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fd6b285b34b77cc441ca20b29e5669803e6ae1f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1a39123c738f1a279c327588dcf40a4821d5502")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog.Service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog.Service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
